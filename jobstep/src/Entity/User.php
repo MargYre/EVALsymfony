7effaabfe6b841cba3316397297f2fd427d538eb
Margaux <margaux.tarayre@gmail.com>
2025-05-14 15:19:09 +0200

    login and register page

--- jobstep/src/Entity/User.php
+++ jobstep/src/Entity/User.php
@@ -6,11 +6,13 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -248,15 +250,13 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    public function getNo(): ?string
+    public function getNom(): ?string
     {
-        return $this->no;
+        return $this->nom;
     }
-
-    public function setNo(string $no): static
+    public function setNom(string $nom): static
     {
-        $this->no = $no;
-
+        $this->nom = $nom;
         return $this;
     }
 }
